03e7548ae51693dbac44ee59002091f872803357
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-04 19:12:43 +0530

    Function module

--- Function.py
+++ Function.py
@@ -1,1 +1,8 @@
 import random
+
+
+def hangman():
+    list1 = ["python", "hangman", "computer", "programming", "gaming", "hello", "world"]
+    sec_word = random.choice(list1)
+    guessed = []
+    tries = 6
